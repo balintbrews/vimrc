e9cb6c7879a07582d898b370632a86537683a202
Balint Kleri <balint@kleri.hu>
2012-04-22 16:10:02 +0200

    Use /Users/balint instead of ~, removed unnecessary parameters from the rm commands.

--- uninstall.sh
+++ uninstall.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
-INSTALLED_TO=~
+INSTALLED_TO=${HOME}
 
 rm -rf "$INSTALLED_TO/vimrc"
 echo "~/vimrc folder has been deleted."
-rm -rf "$INSTALLED_TO/.vim"
+rm "$INSTALLED_TO/.vim"
 echo "~/.vim folder has been deleted."
 rm "$INSTALLED_TO/.vimrc"
 echo "~/.vimrc has been deleted."
-
-cd ~
 
 echo "If you want to install again, run the following command inside your home folder:"
 echo "wget -O - https://raw.github.com/balintk/vimrc/master/install.sh | sh"
