394f52df4794eec83256a6d22bbe00a97f6724cd
Balint Kleri <balint@kleri.hu>
2012-04-22 15:49:53 +0200

    Switch folders properly.

--- uninstall.sh
+++ uninstall.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 INSTALLED_TO=~
 
-cd "$INSTALLED_TO"
-rm -rf vimrc
+rm -rf "$INSTALLED_TO/vimrc"
 echo "~/vimrc folder has been deleted."
-rm -rf .vim
+rm -rf "$INSTALLED_TO/.vim"
 echo "~/.vim folder has been deleted."
-rm -rf .vimrc
-cd
+rm "$INSTALLED_TO/.vimrc"
 echo "~/.vimrc has been deleted."
+
+cd ~
+
 echo "If you want to install again, run the following command inside your home folder:"
 echo "wget -O - https://raw.github.com/balintk/vimrc/master/install.sh | sh"
 
