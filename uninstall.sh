084c7313fee5247494ec05652da2e7671aa47829
Balint Kleri <balint@kleri.hu>
2012-04-22 15:24:59 +0200

    Uninstall file for quickly removing every vim-related file.

---
+++ uninstall.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+INSTALLED_TO=~
+
+cd "$INSTALLED_TO"
+rm -rf vimrc
+echo "~/vimrc folder has been deleted."
+rm -rf .vim
+echo "~/.vim folder has been deleted."
+rm -rf .vimrc
+echo "~/.vimrc has been deleted."
+echo "If you want to install again, run the following command:"
+echo "wget -O - https://raw.github.com/balintk/vimrc/master/autoinstall.sh | sh"
+
