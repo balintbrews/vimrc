4c4c7beeb7ed02359fbbaf3f7d266cb81d1e4d25
Balint Kleri <balint@kleri.hu>
2012-11-12 15:39:23 +0100

    Delete the .viminfo file in the uninstall script.

--- uninstall.sh
+++ uninstall.sh
@@ -7,6 +7,8 @@ rm "$INSTALLED_TO/.vim"
 echo "~/.vim folder has been deleted."
 rm "$INSTALLED_TO/.vimrc"
 echo "~/.vimrc has been deleted."
+rm "$INSTALLED_TO/.viminfo"
+echo "~/.viminfo has been deleted."
 
 echo "If you want to install again, run the following command inside your home folder:"
 echo "wget -O - https://raw.github.com/balintk/vimrc/master/install.sh | sh"
